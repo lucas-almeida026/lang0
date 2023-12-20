66704bc437e560f724f30ae1e958951500a3e8fa
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2023-12-20 15:11:11 -0300

    post 5

--- lang0.js
+++ lang0.js
@@ -4,41 +4,62 @@
 function id(x) { return x[0]; }
 
 const tokenizer = require('./tokenizer')
+
+function binary_expression(data) {
+  return {
+    type: 'binary_expression',
+    operator: data[2],
+    left: data[0],
+    right: data[4]
+  }
+}
 var grammar = {
     Lexer: tokenizer,
     ParserRules: [
-    {"name": "program", "symbols": ["literal"], "postprocess": id},
-    {"name": "program", "symbols": ["term_expression"], "postprocess": id},
-    {"name": "program", "symbols": ["factor_expression"], "postprocess": id},
-    {"name": "program", "symbols": ["unary_expression"], "postprocess": id},
+    {"name": "program", "symbols": ["logical_or_expression"], "postprocess": id},
     {"name": "literal", "symbols": [(tokenizer.has("int") ? {type: "int"} : int)], "postprocess": id},
     {"name": "literal", "symbols": [(tokenizer.has("float") ? {type: "float"} : float)], "postprocess": id},
     {"name": "literal", "symbols": [(tokenizer.has("string") ? {type: "string"} : string)], "postprocess": id},
     {"name": "literal", "symbols": [(tokenizer.has("char") ? {type: "char"} : char)], "postprocess": id},
     {"name": "literal", "symbols": [(tokenizer.has("bool") ? {type: "bool"} : bool)], "postprocess": id},
+    {"name": "comparison_operator", "symbols": [(tokenizer.has("gt") ? {type: "gt"} : gt)], "postprocess": id},
+    {"name": "comparison_operator", "symbols": [(tokenizer.has("gte") ? {type: "gte"} : gte)], "postprocess": id},
+    {"name": "comparison_operator", "symbols": [(tokenizer.has("lt") ? {type: "lt"} : lt)], "postprocess": id},
+    {"name": "comparison_operator", "symbols": [(tokenizer.has("lte") ? {type: "lte"} : lte)], "postprocess": id},
+    {"name": "equality_operator", "symbols": [(tokenizer.has("eqeq") ? {type: "eqeq"} : eqeq)], "postprocess": id},
+    {"name": "equality_operator", "symbols": [(tokenizer.has("neq") ? {type: "neq"} : neq)], "postprocess": id},
     {"name": "term_operator", "symbols": [(tokenizer.has("plus") ? {type: "plus"} : plus)], "postprocess": id},
     {"name": "term_operator", "symbols": [(tokenizer.has("dash") ? {type: "dash"} : dash)], "postprocess": id},
     {"name": "factor_operator", "symbols": [(tokenizer.has("star") ? {type: "star"} : star)], "postprocess": id},
     {"name": "factor_operator", "symbols": [(tokenizer.has("slash") ? {type: "slash"} : slash)], "postprocess": id},
     {"name": "unary_operator", "symbols": [(tokenizer.has("bang") ? {type: "bang"} : bang)], "postprocess": id},
     {"name": "unary_operator", "symbols": [(tokenizer.has("dash") ? {type: "dash"} : dash)], "postprocess": id},
-    {"name": "term_expression", "symbols": ["literal", "__", "term_operator", "__", "literal"], "postprocess":  data => ({
-          type: 'binary_expression',
-          operator: data[2],
-          left: data[0],
-          right: data[4],
-        }) },
-    {"name": "factor_expression", "symbols": ["literal", "__", "factor_operator", "__", "literal"], "postprocess":  data => ({
-          type: 'binary_expression',
-          operator: data[2],
-          left: data[0],
-          right: data[4],
-        }) },
-    {"name": "unary_expression", "symbols": ["unary_operator", "literal"], "postprocess":  data => ({
+    {"name": "primary_expression", "symbols": ["literal"], "postprocess": id},
+    {"name": "primary_expression", "symbols": [(tokenizer.has("lparen") ? {type: "lparen"} : lparen), "term_expression", (tokenizer.has("rparen") ? {type: "rparen"} : rparen)], "postprocess": data => data[1]},
+    {"name": "unary_expression", "symbols": ["primary_expression"], "postprocess": id},
+    {"name": "unary_expression", "symbols": ["unary_operator", "primary_expression"], "postprocess":  data => ({
           type: 'unary_expression',
           operator: data[0],
           argument: data[1],
         }) },
+    {"name": "factor_expression", "symbols": ["unary_expression"], "postprocess": id},
+    {"name": "factor_expression", "symbols": ["factor_expression", "__", "factor_operator", "__", "factor_expression"], "postprocess": binary_expression},
+    {"name": "term_expression", "symbols": ["factor_expression"], "postprocess": id},
+    {"name": "term_expression", "symbols": ["term_expression", "__", "term_operator", "__", "term_expression"], "postprocess": binary_expression},
+    {"name": "comparison_expression", "symbols": ["term_expression"], "postprocess": id},
+    {"name": "comparison_expression", "symbols": ["comparison_expression", "__", "comparison_operator", "__", "comparison_expression"], "postprocess": binary_expression},
+    {"name": "equality_expression", "symbols": ["comparison_expression"], "postprocess": id},
+    {"name": "equality_expression", "symbols": ["equality_expression", "__", "equality_operator", "__", "equality_expression"], "postprocess": binary_expression},
+    {"name": "bitwise_and_expression", "symbols": ["equality_expression"], "postprocess": id},
+    {"name": "bitwise_and_expression", "symbols": ["bitwise_and_expression", "__", (tokenizer.has("and") ? {type: "and"} : and), "__", "equality_expression"], "postprocess": binary_expression},
+    {"name": "bitwise_xor_expression", "symbols": ["bitwise_and_expression"], "postprocess": id},
+    {"name": "bitwise_xor_expression", "symbols": ["bitwise_xor_expression", "__", (tokenizer.has("caret") ? {type: "caret"} : caret), "__", "bitwise_and_expression"], "postprocess": binary_expression},
+    {"name": "bitwise_or_expression", "symbols": ["bitwise_xor_expression"], "postprocess": id},
+    {"name": "bitwise_or_expression", "symbols": ["bitwise_or_expression", "__", (tokenizer.has("or") ? {type: "or"} : or), "__", "bitwise_xor_expression"], "postprocess": binary_expression},
+    {"name": "logical_and_expression", "symbols": ["bitwise_or_expression"], "postprocess": id},
+    {"name": "logical_and_expression", "symbols": ["logical_and_expression", "__", (tokenizer.has("lor") ? {type: "lor"} : lor), "__", "bitwise_or_expression"], "postprocess": binary_expression},
+    {"name": "logical_or_expression", "symbols": ["logical_and_expression"], "postprocess": id},
+    {"name": "logical_or_expression", "symbols": ["logical_or_expression", "__", (tokenizer.has("land") ? {type: "land"} : land), "__", "logical_and_expression"], "postprocess": binary_expression},
     {"name": "_$ebnf$1", "symbols": []},
     {"name": "_$ebnf$1", "symbols": ["_$ebnf$1", (tokenizer.has("WS") ? {type: "WS"} : WS)], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "_", "symbols": ["_$ebnf$1"], "postprocess": id},
--- lang0.ne
+++ lang0.ne
@@ -1,14 +1,20 @@
 @{%
 const tokenizer = require('./tokenizer')
+
+function binary_expression(data) {
+  return {
+    type: 'binary_expression',
+    operator: data[2],
+    left: data[0],
+    right: data[4]
+  }
+}
 %}
 
 @lexer tokenizer
 
 program
-  -> literal {% id %}
-  | term_expression {% id %}
-  | factor_expression {% id %}
-  | unary_expression {% id %}
+  -> logical_or_expression {% id %}
 
 literal
   -> %int {% id %}
@@ -17,6 +23,15 @@ literal
   | %char {% id %}
   | %bool {% id %}
 
+comparison_operator
+  -> %gt {% id %}
+  | %gte {% id %}
+  | %lt {% id %}
+  | %lte {% id %}
+
+equality_operator
+  -> %eqeq {% id %}
+  | %neq {% id %}
 
 term_operator
   -> %plus {% id %}
@@ -30,29 +45,57 @@ unary_operator
   -> %bang {% id %}
   | %dash {% id %}
 
-term_expression
-  -> literal __ term_operator __ literal {% data => ({
-    type: 'binary_expression',
-    operator: data[2],
-    left: data[0],
-    right: data[4],
-  }) %}
-
-factor_expression
-  -> literal __ factor_operator __ literal {% data => ({
-    type: 'binary_expression',
-    operator: data[2],
-    left: data[0],
-    right: data[4],
-  }) %}
+primary_expression
+  -> literal {% id %}
+  | %lparen term_expression %rparen {% data => data[1] %}
 
 unary_expression
-  -> unary_operator literal {% data => ({
+  -> primary_expression {% id %}
+  | unary_operator primary_expression {% data => ({
     type: 'unary_expression',
     operator: data[0],
     argument: data[1],
   }) %}
 
+factor_expression
+  -> unary_expression {% id %}
+  | factor_expression __ factor_operator __ factor_expression {% binary_expression %}
+
+term_expression
+  -> factor_expression {% id %}
+  | term_expression __ term_operator __ term_expression {% binary_expression %}
+
+comparison_expression
+  -> term_expression {% id %}
+  | comparison_expression __ comparison_operator __ comparison_expression {% binary_expression %}
+
+equality_expression
+  -> comparison_expression {% id %}
+  | equality_expression __ equality_operator __ equality_expression {% binary_expression %}
+
+# bitwise start
+bitwise_and_expression
+  -> equality_expression {% id %}
+  | bitwise_and_expression __ %and __ equality_expression {% binary_expression %}
+
+bitwise_xor_expression
+  -> bitwise_and_expression {% id %}
+  | bitwise_xor_expression __ %caret __ bitwise_and_expression {% binary_expression %}
+
+bitwise_or_expression
+  -> bitwise_xor_expression {% id %}
+  | bitwise_or_expression __ %or __ bitwise_xor_expression {% binary_expression %}
+# bitwise end
+
+# logical start
+logical_and_expression
+  -> bitwise_or_expression {% id %}
+  | logical_and_expression __ %lor __ bitwise_or_expression {% binary_expression %}
+
+logical_or_expression
+  -> logical_and_expression {% id %}
+  | logical_or_expression __ %land __ logical_and_expression {% binary_expression %}
+# logical end
 
 _ -> %WS:* {% id %}
 __ -> %WS:+ {% id %}
